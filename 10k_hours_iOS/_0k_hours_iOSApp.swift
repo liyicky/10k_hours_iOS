46dbe8a9a559c528571700eecf719a53ed3aa78c
liyicky <jason@liyicky.com>
2022-12-22 10:24:25 +0900

    Initial Commit

---
+++ 10k_hours_iOS/_0k_hours_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0k_hours_iOSApp.swift
+//  10k_hours_iOS
+//
+//  Created by Jason Cheladyn on 2022/12/22.
+//
+
+import SwiftUI
+
+@main
+struct _0k_hours_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
